3392857aa2c0fa332089bc4d5b27e2395df8f034
dhafer1hmidi <hmididhafer@yahoo.com>
2021-10-20 11:45:42 +0200

    after the home page is added

--- angular/src/app/admin/sidebare/sidebare.component.ts
+++ angular/src/app/admin/sidebare/sidebare.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebare.component.css']
 })
 export class SidebareComponent implements OnInit {
-
+//comontaire
   constructor() { }
 
   ngOnInit(): void {
