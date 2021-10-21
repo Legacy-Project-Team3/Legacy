6abbcde438fbe3bd35b4b7de8fd6f03e3c04c8b7
rimamenaa <rimamenaa98@gmail.com>
2021-10-21 16:43:45 +0100

    auth in prog

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
@@ -104,6 +105,7 @@ FullCalendarModule.registerPlugins([
     BrowserModule,
     FullCalendarModule,
     ReactiveFormsModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
