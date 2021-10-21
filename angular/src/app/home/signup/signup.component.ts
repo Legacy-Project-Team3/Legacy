3c109e55af13fcabba9e786a45d9f1e2ed5f9284
rimamenaa <rimamenaa98@gmail.com>
2021-10-21 21:57:49 +0100

    auth in progress

--- angular/src/app/home/signup/signup.component.ts
+++ angular/src/app/home/signup/signup.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormGroup,
-  Validators
-} from '@angular/forms';
-import Validation from '../../utils/validation';
 
 @Component({
   selector: 'app-signup',
@@ -18,57 +11,13 @@ export class SignupComponent implements OnInit {
   email : string;
   password : string;
 
-  formi: FormGroup;
-  submitted = false;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.formi = this.formBuilder.group(
-      {
-        fullname: ['', Validators.required],
-        username: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(20)
-          ]
-        ],
-        email: ['', [Validators.required, Validators.email]],
-        password: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(40)
-          ]
-        ],
-        confirmPassword: ['', Validators.required],
-        acceptTerms: [false, Validators.requiredTrue]
-      },
-      {
-        validators: [Validation.match('password', 'confirmPassword')]
-      }
-    );
   }
-  get f(): { [key: string]: AbstractControl } {
-    return this.formi.controls;
-  }
-// onSubmit(form: NgForm){
-//  console.log(form.value)
-// }
-onSubmit(): void {
-  this.submitted = true;
-
-  if (this.formi.invalid) {
-    return;
-  }
-
-  console.log(JSON.stringify(this.formi.value, null, 2));
+onSubmit(form: NgForm){
+ console.log(form.value)
 }
-onReset(): void {
-  this.submitted = false;
-  this.formi.reset();
-}
+
 }
