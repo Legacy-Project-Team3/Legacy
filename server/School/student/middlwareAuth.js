9d15da46272199380334edb907551216901ac02f
firas-debich <debichf02@gmail.com>
2021-10-21 23:00:19 +0100

    done

--- server/School/student/middlwareAuth.js
+++ server/School/student/middlwareAuth.js
@@ -3,7 +3,7 @@ const config = process.env;
 
 const verifyToken = async(req,res,next)=>{
 
-    const token =req.body.token || req.query.token || req.headers["x-access-token"];
+    const token =req.body.token || req.query.token || req.headers["authorization"];
     if (!token) {
         return res.status(403).send("A token is required for authentication");
       }
