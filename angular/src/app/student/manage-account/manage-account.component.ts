212f0b9e1761545b38238ea42253ed33bc7700db
firas-debich <debichf02@gmail.com>
2021-10-22 23:09:02 +0100

    small issues with update

--- angular/src/app/student/manage-account/manage-account.component.ts
+++ angular/src/app/student/manage-account/manage-account.component.ts
@@ -1,6 +1,8 @@
+import { tokenize } from '@angular/compiler/src/ml_parser/lexer';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserserviceService } from '../../services/userservice.service';
+import { JwtHelperService } from "@auth0/angular-jwt";
 @Component({
   selector: 'app-manage-account',
   templateUrl: './manage-account.component.html',
@@ -19,9 +21,12 @@ export class ManageAccountComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  onSubmit(form: NgForm){
-    console.log(form.value)
-  this.userservice.managaAccount(form.value).subscribe(res=>{
+  onSubmit(form: NgForm ){
+    const  token= localStorage.getItem("acces_token")
+    const helper = new JwtHelperService();
+    const Id = helper.decodeToken(localStorage.getItem("acces_token"))
+    console.log(Id.student_id)
+  this.userservice.managaAccount(form.value ,token,Id.student_id).subscribe(res=>{
     console.log(res)
   })
 }
