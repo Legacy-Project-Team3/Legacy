c62b8da87a79a74796ba4a1318218f97036fe3e2
dhafer1hmidi <hmididhafer@yahoo.com>
2021-10-23 11:18:22 +0200

    solved conflict

--- angular/src/app/student/sign-in/sign-in.component.ts
+++ angular/src/app/student/sign-in/sign-in.component.ts
@@ -1,33 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { AuthService } from '../../auth.service';
-import { Router } from '@angular/router';
-=======
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import {UserserviceService} from "../../services/userservice.service"
->>>>>>> 92cfa6f4dc2dd313766b26e59ee1e161fd29364d
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponentStudent implements OnInit {
-<<<<<<< HEAD
-  userEmail : String;
-  userPassword : String;
-  constructor(private authService : AuthService, private router : Router) { }
-
-  ngOnInit() {
-  }
-  signin(){
-    this.authService.validate(this.userEmail, this.userPassword)
-    .then((response) => {
-      this.authService.setUserInfo({'user' : response['user']});
-      this.router.navigate(['home']);
-
-    })
-=======
   Email:String;
   Password:string;
 
@@ -41,6 +21,5 @@ export class SignInComponentStudent implements OnInit {
         localStorage.setItem("acces_token",JSON.stringify(res))
         this.router.navigate(["../student"])
       })
->>>>>>> 92cfa6f4dc2dd313766b26e59ee1e161fd29364d
   }
 }
